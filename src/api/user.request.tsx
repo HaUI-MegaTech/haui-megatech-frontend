ab4c076a138f298547de14bbf327f58bdb767619
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-04-24 22:25:00 +0700

    add user request api

--- src/api/user.request.tsx
+++ src/api/user.request.tsx
@@ -5,6 +5,15 @@ class HandleProducts {
   getProducts = async () => {
     return axiosClient.get(BASE_URL + 'products')
   }
+  getProductById = async (id : string) => {
+    return axiosClient.get(BASE_URL + `products/${id}`)
+  }
+  getListBrands = async () => {
+    return axiosClient.get(BASE_URL + 'brands/active')
+  }
+  getProductsByBrandId = async (id: string) => {
+    return axiosClient.get(BASE_URL + `products/active/${id}`)
+  }
 }
 
 const handleProducts = new HandleProducts()
