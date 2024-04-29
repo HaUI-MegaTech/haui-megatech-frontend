40664575f1bba06cd08d439447b71ab37e54aea3
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-04-29 23:32:58 +0700

    add global store management

---
+++ src/store/store.ts
@@ -0,0 +1,40 @@
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { ProductDetail } from '@/types/property.types'
+import { create } from 'zustand'
+
+export type ProductState = {
+  products: ProductDetail[],
+  addProductToCompare: (product: ProductDetail) => void
+  removeProductInCompare: (product: ProductDetail) => void
+}
+
+export const useProductCompareStore = create<ProductState>()(
+  persist(
+    (set, get) => ({
+      products: [],
+      addProductToCompare: (product) => {
+        const currentProducts = get().products;
+        let index = currentProducts.findIndex(prod => prod.id === product.id);
+        if (currentProducts.length >= 3) {
+          alert('Xóa bớt danh sách sản phẩm trong danh sách so sánh')
+          return;
+        } else {
+          if (index != -1) {
+            alert('Sản phẩm đã có trong danh sách so sánh');
+            return;
+          }
+        }
+        set({
+          products: [...currentProducts, product]
+        });
+      },
+      removeProductInCompare: (product: ProductDetail) =>
+        set(state => ({ products: state.products.filter(prod => prod.id !== product.id) })),
+    }),
+    {
+      name: "haui-megatech-compare",
+      storage: createJSONStorage(() => localStorage),
+      skipHydration: true
+    }
+  )
+)
