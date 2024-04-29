a77d2e26e3bd2ade30f9a5994405aeef1d28c797
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-04-29 23:47:50 +0700

    add link to detail page from card product

--- src/components/app.card.tsx
+++ src/components/app.card.tsx
@@ -1,63 +1,66 @@
-'use client'
+// 'use client'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import styles from '@/styles/card.module.scss'
 import { Product } from '@/types/property.types'
+import Link from 'next/link'
 
 const AppCard = ({ product }: { product: Product }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.image}>
-        <Image
-          src={product.mainImg}
-          alt="image"
-          width={0}
-          height={0}
-          objectFit='contain'
-          sizes='100vw'
-          style={{ width: '100%', height: 'auto' }} />
-      </div>
-      <div className='product-info'>
-        <div className={styles.title}>
-          {product.name}
+    <Link href={`/product/${product.id}`}>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <Image
+            src={product.mainImg ? product.mainImg : ""}
+            alt="image"
+            width={0}
+            height={0}
+            objectFit='contain'
+            sizes='100vw'
+            style={{ width: '100%', height: 'auto' }} />
         </div>
-        <div className='product-promotion'>
-          <table className={styles.table}>
-            <tbody>
-              <tr>
-                <td width={50}>CPU</td>
-                <td>{product.processor}</td>
-              </tr>
-              <tr>
-                <td>RAM</td>
-                <td>{product.ram}</td>
-              </tr>
-              <tr>
-                <td>Bộ nhớ</td>
-                <td>{product.storage}</td>
-              </tr>
-              <tr>
-                <td>Card</td>
-                <td>{product.card}</td>
-              </tr>
-              <tr>
-                <td>M.hình</td>
-                <td>{product.display}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className={styles.price}>
-          <div className={styles.top}>
-            <del className={styles.oldPrice}>{(new Intl.NumberFormat('vi-VN').format(product.oldPrice))}đ</del>
-            <span className={styles.sale}>-{product.discountPercent}%</span>
+        <div className='product-info'>
+          <div className={styles.title}>
+            {product.name}
           </div>
-          <div className={styles.bottom}>
-            <span className={styles.priceMain}>{(new Intl.NumberFormat('vi-VN').format(product.newPrice))}đ</span>
+          <div className='product-promotion'>
+            <table className={styles.table}>
+              <tbody>
+                <tr>
+                  <td width={50}>CPU</td>
+                  <td>{product.processor}</td>
+                </tr>
+                <tr>
+                  <td>RAM</td>
+                  <td>{product.ram}</td>
+                </tr>
+                <tr>
+                  <td>Bộ nhớ</td>
+                  <td>{product.storage}</td>
+                </tr>
+                <tr>
+                  <td>Card</td>
+                  <td>{product.card}</td>
+                </tr>
+                <tr>
+                  <td>M.hình</td>
+                  <td>{product.display}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className={styles.price}>
+            <div className={styles.top}>
+              <del className={styles.oldPrice}>{(new Intl.NumberFormat('vi-VN').format(product.oldPrice))}đ</del>
+              <span className={styles.sale}>-{product.discountPercent}%</span>
+            </div>
+            <div className={styles.bottom}>
+              <span className={styles.priceMain}>{(new Intl.NumberFormat('vi-VN').format(product.newPrice))}đ</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
