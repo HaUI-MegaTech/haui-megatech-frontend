738063dd5dbf75334077a79a7c352c1ec0942992
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-04-29 23:48:07 +0700

    change config axios

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,27 +1,29 @@
-import axios from "axios";
+import { ProductDetail } from "@/types/property.types";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+interface MyResponseData {
+  item: ProductDetail; 
+}
 
-const axiosClient = axios.create()
+const axiosClient = axios.create();
 
-axiosClient.interceptors.request.use(async( config: any) => {
+axiosClient.interceptors.request.use(async (config: AxiosRequestConfig) => {
   config.headers = {
     'Content-type': 'application/json',
     ...config.headers,
-  },
+  };
 
-  config.data
+  return config;
+});
 
-  return config
-})
-
-axiosClient.interceptors.response.use((response) => {
+axiosClient.interceptors.response.use((response: AxiosResponse<MyResponseData>) => {
   if (response.status === 200 && response.data) {
-    return response.data
+    return response.data;
   }
 
-  return response
-},
-error => {
-  console.warn(`Lỗi kết nối đến cơ sở dữ liệu, ${error.message}`)
-})
+  return response;
+}, error => {
+  console.warn(`Lỗi kết nối đến cơ sở dữ liệu, ${error.message}`);
+  return Promise.reject(error);
+});
 
-export default axiosClient
+export default axiosClient;
