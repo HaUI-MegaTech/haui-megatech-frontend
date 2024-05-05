4071dfff1a8804d79b5e6a7e07b32a4c66fc2f43
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-05-05 08:09:36 +0700

    🐛 change request url of get products

--- src/api/user.request.tsx
+++ src/api/user.request.tsx
@@ -5,14 +5,20 @@ class HandleProducts {
   getProducts = async () => {
     return axiosClient.get(BASE_URL + 'products/active')
   }
-  getProductById = async (id : string) => {
+  getProductById = async (id: string) => {
     return axiosClient.get(BASE_URL + `products/${id}`)
   }
   getListBrands = async () => {
     return axiosClient.get(BASE_URL + 'brands/active')
   }
-  getProductsByBrandId = async (query: string) => {
-    return axiosClient.get(BASE_URL + `products/active/${query}`)
+  getProductsByBrandId = async (id:string, query: string) => {
+    return axiosClient.get(BASE_URL + `brands/${id}/products/active`)
+  }
+  getProductsSearch = async (query: string) => {
+    return axiosClient.get(BASE_URL + `products/active?${query}`)
+  }
+  getProductsFromOtherWebsites = async (query: string) => {
+    return axiosClient.get(`http://localhost:8080/search?keyword=${query}`)
   }
 }
 
