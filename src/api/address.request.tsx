0dcd4bc3f0a77b899813fdb5b5e62a93c0345ba8
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-05-05 09:04:01 +0700

    add request api of provinces, districts, wards in vietnam

--- src/api/address.request.tsx
+++ src/api/address.request.tsx
@@ -1,16 +1,17 @@
 import axiosClient from "./config"
 
-const BASE_URL = "https://vn-public-apis.fpo.vn/"
+const BASE_URL = "web-staging.ghtklab.com/api/v1/public/address/list"
 class HandleAddress {
   getAllProvinces = async () => {
-    return axiosClient.post(BASE_URL + 'provinces/getAll?limit=-1')
+    return axiosClient.post(BASE_URL)
   }
   getAllDistrictsByProvinces = async (code : number) => {
-    return axiosClient.post(BASE_URL + `districts/getByProvince?provinceCode=${code}&limit=-1`)
+    return axiosClient.post(BASE_URL + `?parentId=${code}&type=3`)
   }
   getAllWardsByDistricts = async (code : number) => {
-    return axiosClient.post(BASE_URL + `wards/getByDistrict?districtCode=${code}&limit=-1`)
+    return axiosClient.post(BASE_URL + `parentId=${code}&type=1`)
   }
+  
 }
 
 const handleAddress = new HandleAddress()
