274901251fec42184b655867795f231c1b36fc26
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-05-13 09:26:13 +0700

    re-config cart request url

--- src/api/cart.request.tsx
+++ src/api/cart.request.tsx
@@ -1,18 +1,20 @@
 import { ItemAddCart } from "@/types/property.types"
 import axiosClient from "./config"
-
+import axios from "axios";
+const cartUrl = "cart-items"
+let idUser = JSON.parse(localStorage.getItem('haui-megatech-user-infor')).id;
 class HandleCart {
   getCart = async () => {
-    return axiosClient.get()
+    return axiosClient.get(cartUrl)
   }
   addToCart = async (data: ItemAddCart) => {
-    return axiosClient.post(data)
+    return axiosClient.post("cart-items", data)
   }
   deleteItems = async (data: string) => {
-    return axiosClient.delete("/" + data);
+    return axiosClient.delete(`cart-items/${data}`);
   }
   updateItem = async (data) => {
-    return axiosClient.put(`/${data.cartItemId}`, {
+    return axiosClient.put(`cart-items/${data.cartItemId}`, {
       productId: data.productId,
       quantity: data.quantity
     });
