2f30bd0fc1f609ebe3214768e55dd7fd076faa2a
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-05-24 11:35:19 +0700

    add filter products

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,42 +3,53 @@ import { Button, Checkbox, Col, Divider, Form, InputNumber, Pagination, Rate, Ro
 import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss'
 import AppCard from '@/components/app.card';
-import { ListBrand, Product } from '@/types/property.types';
+import { Brand, ListBrand, Product } from '@/types/property.types';
 import handleProducts from '@/api/user.request';
 import { useRouter, useSearchParams } from 'next/navigation';
-
+enum SortBy {
+  TOTAL_SOLD = 'totalSold',
+  UPDATED_AT = 'updatedAt',
+  PRICE = 'price',
+  DECREASE_PRICE = '-price'
+}
 const SearchPage = () => {
   const items = [
     {
-      key: '&sort=-sold',
+      key: SortBy.TOTAL_SOLD,
       label: 'Phổ biến',
     },
     {
-      key: '&sort=-updatedAt',
+      key: SortBy.UPDATED_AT,
       label: 'Mới nhất',
     },
     {
-      key: '&sort=price',
+      key: SortBy.PRICE,
       label: 'Giá thấp đến cao',
     },
     {
-      key: '&sort=-price',
+      key: SortBy.DECREASE_PRICE,
       label: 'Giá cao đến thấp',
     },
   ];
   const [listProduct1, setListProduct1] = useState<Product[]>([]);
-  const [query, setQuery] = useState<string>();
-  const [pageIndex, setPageIndex] = useState(0);
-  const [pageSize, setPageSize] = useState(16);
-  const [totalItem, setTotalItem] = useState(10);
   const searchParams = useSearchParams();
-  const brandId: string | any = searchParams.get('brand');
-  const router = useRouter();
-  const searchTerm = router.query || 'laptop';
+  const brand: string | any = searchParams.get('brand');
+  const [brandIds, setBrandIds] = useState<string>(brand);
+  const [pageIndex, setPageIndex] = useState<number>(0);
+  const [pageSize, setPageSize] = useState<number>(16);
+  const [sortBy, setSortBy] = useState<string>('-totalSold');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(30000000);
+  const [orderBy, setOrderBy] = useState('asc')
+  const [query, setQuery] = useState<string>(`brandIds=${brandIds}&index=${pageIndex}&limit=${pageSize}&field=${sortBy}&minPrice=${fromPrice}&maxPrice=${toPrice}&direction=${orderBy}`);
+  const [totalItem, setTotalItem] = useState(10);
+
+  //const router = useRouter()
+  //const searchTerm = router.query || 'laptop';
   const handleGetProducts = async () => {
     try {
-      const searchQuery = `brandIds=${brandId}&minPrice=0&maxPrice=0&index=${pageIndex}&limit=${pageSize}`;
-      const res = await handleProducts.getActiveProducts(searchQuery);
+      setQuery(`brandIds=${brandIds}&index=${pageIndex}&limit=${pageSize}&field=${sortBy}&minPrice=${fromPrice}&maxPrice=${toPrice}&direction=${orderBy}`);
+      const res: any = await handleProducts.getActiveProducts(query);
       if (res) {
         setListProduct1(res.items);
         setTotalItem(res.totalItems);
@@ -53,9 +64,9 @@ const SearchPage = () => {
   useEffect(() => {
     handleGetProducts();
     document.title = 'Tìm kiếm'
-  }, [searchParams, pageIndex, pageSize])
+  }, [pageIndex, pageSize, sortBy])
 
-  const [listBrand, setListBrand] = useState<ListBrand[]>([]);
+  const [listBrand, setListBrand] = useState<Brand[]>([]);
   const handleGetListBrands = async () => {
     try {
       let res: any = await handleProducts.getListBrands();
@@ -66,7 +77,38 @@ const SearchPage = () => {
       console.log(err);
     }
   }
-
+  const handleFilterProductsByProperty = (key: string) => {
+    console.log(key);
+    switch (key) {
+      case SortBy.TOTAL_SOLD:
+        setSortBy(SortBy.TOTAL_SOLD);
+        setOrderBy('desc');
+        break;
+      case SortBy.UPDATED_AT:
+        setSortBy(SortBy.UPDATED_AT);
+        setOrderBy('desc');
+        break;
+      case SortBy.PRICE:
+        setSortBy(SortBy.PRICE);
+        break;
+      case SortBy.DECREASE_PRICE:
+        setSortBy(SortBy.DECREASE_PRICE);
+        setOrderBy('desc');
+        break;
+    }
+  }
+  const onChangeForm = (changedVal: any, values: any) => {
+    if (!!changedVal?.category) {
+      setBrandIds(values.category.join(','));
+    }
+    if (!!changedVal.range) {
+      setFromPrice(values.range.from);
+      setToPrice(values.range.to);
+    }
+  }
+  const handleApplyForm = async () => {
+    await handleGetProducts();
+  }
   useEffect(() => {
     handleGetListBrands();
   }, [])
@@ -75,7 +117,7 @@ const SearchPage = () => {
       <div className={styles.main}>
         <div className={styles.left}>
           <Form
-          // onValuesChange={(changedValues, values) => onChangeForm(changedValues, values)}
+            onValuesChange={(changedValues, values) => onChangeForm(changedValues, values)}
           >
             <div className={styles.titleMenu}>
               Bộ lọc tìm kiếm
@@ -85,8 +127,8 @@ const SearchPage = () => {
             >
               <Checkbox.Group>
                 <Row>
-                  {listBrand?.map((item) => <Col key={item.id} span={24}>
-                    <Checkbox value={item.name}>{item.name}</Checkbox>
+                  {listBrand?.map((item: Brand) => <Col key={item.id} span={24}>
+                    <Checkbox value={item.id}>{item.name}</Checkbox>
                   </Col>)}
                 </Row>
               </Checkbox.Group>
@@ -104,30 +146,24 @@ const SearchPage = () => {
                 </Form.Item>
               </div>
             </Form.Item>
-            <Button type="primary" block>Áp dụng</Button>
+            <Button type="primary" block onClick={handleApplyForm}>Áp dụng</Button>
             <Divider />
             <p>Đánh giá</p>
-            <div className='review' style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-              <Rate value={5} /> 5 sao
-            </div>
-            <div className='review' style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-              <Rate value={4} /> 4 sao
-            </div>
-            <div className='review' style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-              <Rate value={3} /> 3 sao
-            </div>
-            <div className='review' style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-              <Rate value={2} /> 2 sao
-            </div>
-            <div className='review' style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-              <Rate value={1} /> 1 sao
-            </div>
+            {[5, 4, 3, 2, 1].map((item: number) => {
+              return <div key={item} className='review' style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+                <Rate value={item} /> {item} sao
+              </div>
+            })}
           </Form>
         </div>
         <div className={styles.right}>
           <Spin spinning={false} tip="...Loading">
             <div className={styles.bookDisplay}>
-              <Tabs defaultActiveKey="1" items={items} />
+              <Tabs
+                defaultActiveKey={SortBy.TOTAL_SOLD}
+                items={items}
+                onChange={handleFilterProductsByProperty}
+              />
               <div className={styles.listBook}>
                 {
                   listProduct1?.map((item) => {
@@ -140,7 +176,10 @@ const SearchPage = () => {
               <div className={styles.pagination}>
                 <Pagination
                   onChange={changePageIndex}
-                  total={totalItem} current={+pageIndex + 1} pageSize={pageSize} />
+                  total={totalItem}
+                  current={+pageIndex + 1}
+                  pageSize={pageSize}
+                />
               </div>
             </div>
           </Spin>
@@ -150,4 +189,4 @@ const SearchPage = () => {
   )
 }
 
-export default SearchPage
+export default React.memo(SearchPage);
