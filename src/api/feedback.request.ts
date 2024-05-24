cd35293a00ca7d9128b391f3f2a3c07557f0e1ec
MinhAnh-haui <luongminhanh.haui@gmail.com>
2024-05-24 11:31:26 +0700

    add feedback request api

---
+++ src/api/feedback.request.ts
@@ -0,0 +1,20 @@
+import axiosClient from "./config"
+
+class HandleFeedback {
+  getFeedbackByProductId = async (id: number) => {
+    return axiosClient.get(`products/${id}/feedbacks`)
+  }
+  getFeedbackByUserId = async (id: number) => {
+    return axiosClient.get(`users/${id}/feedback`)
+  }
+  postFeedBack = async (id: number) => {
+    return axiosClient.get(`products/${id}/feedback`)
+  }
+  putFeedback = async (productId: number, feedbackId: number) => {
+    return axiosClient.get(`products/${productId}/feedbacks/${feedbackId}`)
+  }
+}
+
+const handleFeedback = new HandleFeedback()
+
+export default handleFeedback
