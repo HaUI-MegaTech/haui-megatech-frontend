4e421ac53dba2484a9e4f0ccc030afb5da5de245
Nguyễn Việt Hoàng <hoangnv.swe@gmail.com>
2024-06-04 06:03:03 +0700

    Commit to deploy

--- src/api/payment.request.ts
+++ src/api/payment.request.ts
@@ -2,7 +2,7 @@ import axiosClient from "./config"
 
 class HandlePayment {
   createPayment = async (data: string) => {
-    return axiosClient.post(`create-payment?ids=${data}&addressId=28`);
+    return axiosClient.get(`create-payment?ids=${data}&addressId=28`);
   }
   callbackPayment = async () => {
     return axiosClient.get('callback-payment');
